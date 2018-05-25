c7de3407a01fdb8695946baebeec502d3fda3ee4
amazinigmech2418 <33498997+amazinigmech2418@users.noreply.github.com>
2018-05-25 19:48:43 -0400

    Create ext.js

---
+++ ext.js
@@ -0,0 +1,47 @@
+var speechdata = new webkitSpeechRecognition();
+var speak = function(text) {
+speechSynthesis.speak(new SpeechSynthesisUtterance(text));
+}
+var data;
+speechdata.onresult = function(e) {data = e.results[0][0].transcript;}
+(function(ext) {
+    // When Scratch closes, do this
+    ext._shutdown = function() {
+        // Nothing here at the moment...
+    };
+ 
+    // Tell Scratch if the extension is ready to do stuff
+    ext._getStatus = function() {
+        return {status: 2, msg: 'Ready'}; // Status 1 = Red, Error // Status 1 = Yellow, Not Ready // Status 2 = Green, Ready
+    };
+ 
+    // Blocks to add to Scratch
+    var descriptor = {
+        blocks: [
+            // Block type, block name, function name
+            ['w', 'get speech', 'gs'],
+            ['w', 'say %s', 'hi' 'say'],
+            ['r','Speech','s']
+        ],
+menus: {
+txt: ['hi','bye','how are you?'],
+superMath: ['radian sine', 'radian cosine', 'radian tangent', 'asin', 'atan', 'acos', 'power'],
+geo: ['latitude','longitude']
+}
+    };
+ 
+    // What blocks do go here
+ext.gs = function() {
+speechdata.start();
+};
+ext.say = function(a) {
+speak(a);
+};
+ext.s = function() {
+return data;
+};
+    
+    // Name of Scratch Extension goes here
+    ScratchExtensions.register('speech', descriptor, ext); 
+     // The name is 'Hello, World!''
+})({});
